9ae043c37d666b760acb5402acded484fe871623
Jonathan Lambrechts <jonathan.lambrechts@uclouvain.be>
2020-03-25 10:43:39 +0100

    no cpp

--- seamesh.c
+++ seamesh.c
@@ -3,18 +3,15 @@
 #include <math.h>
 #include <string.h>
 
-typedef double (mesh_size_cb_t)(double x, double y,double z);
-
-int find_neigh_id(int *t0, int *t1) {
-  for (int i = 0; i<3; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      if(t0[i] == t1[j] && t0[(i+1)%3] == t1[(j+1)%3]) return i;
-      if(t0[i] == t1[(j+1)%3] && t0[(i+1)%3] == t1[j]) return i;
-    }
+void *vector_push(void **p, int *psize, int elem_size) {
+  if (*psize == 0) {
+    *p = malloc(elem_size*2);
   }
-  printf("neighbor not found\n");
-  exit(1);
-  return -1;
+  else if (*psize%2==0) {
+    *p = realloc(*p,(*psize)*2*elem_size);
+  }
+  *psize += 1;
+  return ((char*)*p) + ((*psize-1)*elem_size);
 }
 
 int is_inside(const double p[2], const double a[2], const double b[2], const double c[2]) {
@@ -50,6 +47,7 @@ static int getid3(const int t[3], int i) {
   if(t[1] == i) return 1;
   if(t[2] == i) return 2;
   printf("element not found\n");
+  exit(1);
   return -1;
 }
 
@@ -154,7 +152,7 @@ int *get_cavity(int t, int v, int *tri, int *neigh) {
   return r;
 }
 
-static double optimal_boundary_collapse(int t0, int e0, int *tri, int *neigh, double *x, int **tri_color) {
+static double optimal_boundary_collapse(int t0, int e0, int *tri, int *neigh, double *x, int *tri_color) {
   int v0 = tri[t0*3+e0];
   int v1 = tri[t0*3+(e0+1)%3];
   double d = vdist(&x[v0*2],&x[v1*2]);
@@ -163,7 +161,7 @@ static double optimal_boundary_collapse(int t0, int e0, int *tri, int *neigh, do
   do {
     int iv1 = getid3(&tri[cur*3],v1);
     int n = neigh[cur*3+iv1];
-    if (n == -1 || (*tri_color)[n] != (*tri_color)[cur]) {
+    if (n == -1 || tri_color[n] != tri_color[cur]) {
       d1 = vdist(&x[v1*2],&x[tri[cur*3+(iv1+1)%3]*2]);
       break;
     }
@@ -173,7 +171,7 @@ static double optimal_boundary_collapse(int t0, int e0, int *tri, int *neigh, do
   do {
     int iv0 = getid3(&tri[cur*3],v0);
     int n = neigh[cur*3+(iv0+2)%3];
-    if (n == -1 || (*tri_color)[n] != (*tri_color)[cur]) {
+    if (n == -1 || tri_color[n] != tri_color[cur]) {
       d0 = vdist(&x[v0*2],&x[tri[cur*3+(iv0+2)%3]*2]);
       break;
     }
@@ -266,206 +264,27 @@ static int collapse_edge(int t0, int e0, double xi, int *tri, int *neigh, double
   return 1;
 }
 
-void gen_boundaries(int n_vertices, double *x, int *tag, int n_tri, int *tri, int *neigh_unsorted, double lon, double lat, mesh_size_cb_t *lc, double lcmin, int *n_tri_o, int **tri_o, int **tri_color, int *n_points_o_p, double **x_o_p)
-{
-  double *length = malloc(sizeof(double)*n_vertices);
-  for (int i=0; i<n_vertices; ++i) {
-    length[i] = lc(x[i*2+0],x[i*2+1],0);
-  }
-  //check orientation
-  for (int i=0; i<n_tri; ++i){
-    int *t = tri+i*3;
-    if (triangle_signed_area(x+t[0]*2,x+t[1]*2,x+t[2]*2) < 0) {
-      int sw = t[0];
-      t[0] = t[1];
-      t[1] = sw;
-    }
-  }
-  //build neighbors
-  int *neigh = malloc(sizeof(int)*n_tri*3);
-  for (int i=0; i<n_tri; ++i){
-    for (int j=0 ;j<3; ++j)
-      neigh[i*3+j] = -1;
-    for (int j = 0; j<3; ++j) {
-      int n = neigh_unsorted[i*3+j];
-      if (n<0) continue;
-      neigh[i*3+find_neigh_id(tri+i*3,tri+n*3)] = n;
-    }
-  }
-  // find vertex with first point
-  int first = -1;
-  double xp[2] = {lon,lat};
-  for (int i=0; i<n_tri; ++i) {
-    int *t = tri+i*3;
-    if(is_inside(xp,x+t[0]*2,x+t[1]*2,x+t[2]*2)) {
-      first = i;
-    }
-  }
-  if(first==-1) {
-    printf("no triangle contains the first point\n");
-    exit(1);
-  }
-
-  // color triangles
-  *tri_color = malloc(sizeof(int)*n_tri);
-  for (int i=0; i<n_tri; ++i) {
-    (*tri_color)[i] = 0;
-  }
-  int *stack = malloc(sizeof(int)*n_tri);
-  int stack_p = 0;
-  stack[0] = first;
-  (*tri_color)[first] = 1;
-  while(stack_p >= 0) {
-    int c = stack[stack_p--];
-    for (int j=0; j<3; ++j) {
-      int n = neigh[c*3+j];
-      if (n >=0 && !(*tri_color)[n]) {
-        int i0 = tri[c*3+j];
-        int i1 = tri[c*3+(j+1)%3];
-        double *p0 = x+2*i0;
-        double *p1 = x+2*i1;
-        double d2 = (p1[0]-p0[0])*(p1[0]-p0[0])+(p1[1]-p0[1])*(p1[1]-p0[1]);
-        double lmin = fmin(length[i0],length[i1]);
-        if(d2 > lmin*lmin) {
-          (*tri_color)[n] = 1;
-          stack[++stack_p] = n;
-        }
-      }
-    }
-  }
-  
-  free(stack);
-  int *vcolor = malloc(sizeof(int)*n_vertices);
-  for (int i=0; i<n_vertices; ++i) {
-    vcolor[i] = 0;
-  }
-  for (int i=0; i<n_tri; ++i) {
-    if ((*tri_color)[i] == 1) {
-      for (int j=0; j<3; ++j) {
-        vcolor[tri[i*3+j]] = 1;
-      }
-    }
-  }
-
-  //swap - collapses
-  size_t nops ;
-  int iter = 0;
-  do {
-    iter += 1;
-    nops = 0;
-    for (int i=0;i<n_tri;i++) {
-      int *t = tri+i*3;
-      for (int j=0; j<3 && t[0] != -1;++j) {
-        int n = neigh[i*3+j];
-        if (n != -1 && (*tri_color)[n] == (*tri_color)[i]){
-          //nops += swap(i,j,tri,x,neigh);
-        }
-        int i0 = t[j];
-        int i1 = t[(j+1)%3];
-        if((*tri_color)[i] == 0 && (n == -1 || (*tri_color)[n] !=1)) {
-          // we are outside the domain
-          // and so is our neighbour (if any)
-          // remove point that does not touch the boundary
-          if (vcolor[i1] == 0) {
-            int r = collapse_edge(i,j,1,tri,neigh,x,length,vcolor);
-            nops += r;
-          }
-          else if (vcolor[i0] == 0){
-            int r = collapse_edge(i,j,0,tri,neigh,x,length,vcolor);
-            nops += r;
-          }
-        }
-        if((*tri_color)[i] == 1 && (n == -1 || (*tri_color)[n] ==0)
-            && vdist(&x[i0*2],&x[i1*2]) < 0.7*fmin(length[i0],length[i1])) {
-          // we are on the boundary
-          // collapse edge if too short (and possible)
-              nops += 
-                collapse_edge(i,j,optimal_boundary_collapse(i,j,tri,neigh,x,tri_color),tri,neigh,x,length,vcolor) ||
-                collapse_edge(i,j,0,tri,neigh,x,length,vcolor) || collapse_edge(i,j,1,tri,neigh,x,length,vcolor);
-        }
-      }
-    }
-    printf("nops = %i\n",nops);
-  } while (nops != 0 && iter < 100);
-  for (int i = 0; i < n_tri; ++i) {
-    int *t = &tri[i*3];
-          if(t[0] == -1) continue;
-    if(triangle_signed_area(&x[t[0]*2],&x[t[1]*2],&x[t[2]*2])<0) {
-      printf("invalid triangle !!\n");
-    }
-  }
-
-  free(neigh);
-  free(length);
-  free(vcolor);
-  int *keep_vertices = malloc(sizeof(int)*n_vertices);
-  for (int i = 0; i < n_vertices; ++i)
-    keep_vertices[i] = -1;
-  int n_vertices_o = 0;
-  *n_tri_o = 0;
-  for (int i = 0; i < n_tri; ++i) {
-    int *t = tri+i*3;
-    if (t[0] == -1) continue;
-    *n_tri_o += 1;
-    for (int j = 0; j < 3; ++j){
-      if (keep_vertices[t[j]] == -1) {
-        n_vertices_o += 1;
-        keep_vertices[t[j]] = 1;
-      }
-    }
-  }
-  double *x_o = malloc(sizeof(double)*2*n_vertices_o);
-  n_vertices_o = 0;
-  for (int i = 0; i < n_vertices; ++i) {
-    if(keep_vertices[i] == 1) {
-      keep_vertices[i] = n_vertices_o;
-      x_o[n_vertices_o*2+0] = x[i*2+0];
-      x_o[n_vertices_o*2+1] = x[i*2+1];
-     n_vertices_o += 1;
-    }
-  }
-  *tri_o = malloc((*n_tri_o)*sizeof(int)*3);
-  int *tri_color_o = malloc(sizeof(int)*(*n_tri_o));
-  int i_tri_o = 0;
-  for (int i = 0; i < n_tri; ++i) {
-    int *t = tri+i*3;
-    if (t[0] == -1) continue;
-    int *t_o = *tri_o+i_tri_o*3;
-    for (int j = 0; j<3; ++j) {
-      t_o[j] = keep_vertices[t[j]];
-    }
-    tri_color_o[i_tri_o] = (*tri_color)[i];
-    i_tri_o += 1;
-  }
-  free(*tri_color);
-  *tri_color = tri_color_o;
-  free(keep_vertices);
-  *n_points_o_p = n_vertices_o;
-  *x_o_p = x_o;
-  printf("end gen boundaries\n");
-}
-
 static int hecmp(const void *p0, const void *p1) {
   const int *i0 = (const int *)p0;
   const int *i1 = (const int *)p1;
   return i0[0] == i1[0] ? i0[1]-i1[1] : i0[0]-i1[0];
 }
 
-int *build_neighbours(int n_tri, const int *tri) {
+static int *build_neighbours(int n_tri, const int *tri) {
   int *hedges = malloc(sizeof(int)*4*n_tri*3);
   for (int i = 0; i < n_tri; ++i) {
     const int *t = tri+i*3;
     for (int j = 0; j < 3; ++j) {
       int ihe = i*3+j;
       int *he = hedges + ihe*4;
-      int i1 = (i+1)%3;
-      if(t[i1] < t[i]) {
-        he[0] = t[i1];
-        he[1] = t[i];
+      int j1 = (j+1)%3;
+      if(t[j1] < t[j]) {
+        he[0] = t[j1];
+        he[1] = t[j];
       }
       else {
-        he[0] = t[i];
-        he[1] = t[i1];
+        he[0] = t[j];
+        he[1] = t[j1];
       }
       he[2] = i;
       he[3] = j;
@@ -482,74 +301,193 @@ int *build_neighbours(int n_tri, const int *tri) {
     if (hecmp(he0,he1) == 0) {
       neighbours[he0[2]*3+he0[3]] = he1[2];
       neighbours[he1[2]*3+he1[3]] = he0[2];
+      i++;
     }
   }
   free(hedges);
   return neighbours;
 }
 
-void write_geo(const char *filename, double lc, int n_tri, const int *tri, const int *tri_color, int n_points, const double *x_p, const int *vtag/*, std::map<int,std::string> boundary_names*/)
+static void extract_boundaries(const double *x_p, const int *vtag, int n_tri, const int *tri, const int *tri_color, const int *neighbours/*, std::map<int,std::string> boundary_names*/,double **xo, int *nxo, int **l, int *nl, int **ll, int *nll)
 {
-  int *neighbours = build_neighbours(n_tri, tri);
-  FILE *f = fopen(filename,"w");
+  *xo = NULL;
+  *ll = NULL;
+  *l = NULL;
+  *nxo = 0, *nll = 0, *nl = 0;
   //std::map<std::string,std::vector<int> > physicals;
-  fprintf(f,"mesh.lcintegrationprecision = 1e-4;\n");
-  fprintf(f,"lc=%g;\n",lc);
-  size_t ip = 1;
-  size_t il = 1;
-  size_t ill = 1;
   char *touched = malloc(sizeof(char)*n_tri);
   for (int i = 0; i < n_tri; ++i) {
     touched[i] = 0;
   }
   for (int i=0;i<n_tri;i++){
     const int *t = tri+3*i;
-    if(tri_color[i] != 0 || touched[i]) continue;
+    if(t[0] == -1 || tri_color[i] != 1 || touched[i]) continue;
     for (int j=0; j<3; ++j) {
       int neigh = neighbours[i*3+j];
-      if (neigh == -1 || tri_color[neigh] || touched[neigh]) continue;
-      int firstp = ip;
-      int firstpll = ip;
-      int firstl = il;
+      if (neigh == -1 || tri_color[neigh]== 1 || touched[neigh]) continue;
+      int firstp = *nxo;
+      int firstpll = *nxo;
+      int firstl = *nl;
       int firstv = t[j], curv = t[j];
       int curt = i;
       int iv = j;
       int oldtag = -1;
       do {
+        t = tri+3*curt;
         touched[curt] = 1;
-        fprintf(f,"point(%i)={%.16g,%.16g,%.16g};\n",ip++,x_p[curv*3+0],x_p[curv*3+1],x_p[curv*3+2]);
-        int tag = vtag[curv];
+        double *X = (double*)vector_push((void**)xo,nxo,sizeof(double)*2);
+        X[0] = x_p[curv*2+0];
+        X[1] = x_p[curv*2+1];
+        int tag = 1;//vtag[curv];
         if (oldtag != -1 && oldtag != tag) {
-          //physicals[boundary_names[oldtag]].push_back(il);
-          fprintf(f,"spline(%i)={%i:%i};\n",il++,firstp,ip-1);
-          firstp = ip-1;
+          firstp = *nxo-1;
         }
         oldtag = tag;
         iv = (iv+1)%3;
         curv = t[iv];
-        while(tri_color[neighbours[curt*3+iv]] == 0) {
+        while(neighbours[curt*3+iv]!= -1 && tri_color[neighbours[curt*3+iv]] == 1) {
           curt = neighbours[curt*3+iv];
           iv = getid3(tri+curt*3,curv);
         }
       }while(curv != firstv);
       //physicals[boundary_names[oldtag]].push_back(il);
-      fprintf(f,"spline(%i)={%i:%i,%i};\n",il++,firstp,ip-1,firstpll);
-      fprintf(f,"line loop(%i={%i:%i};\n",firstl,il-1);
-      il++;
+      int *line = (int*)vector_push((void**)l,nl,3*sizeof(int));
+      line[0] = firstp;
+      line[1] = *nxo-1;
+      line[2] = firstpll;
+      int *lineloop = (int*)vector_push((void**)ll,nll,2*sizeof(int));
+      lineloop[0] = firstl;
+      lineloop[1] = *nl-1;
     }
   }
-  /*f << "physical surface(\"domain\") = {1};\n";
-  for(auto p: physicals) {
-    f << "physical line(\"" << p.first << "\") = {";
-    for(size_t i = 0; i < p.second.size()-1; ++i) {
-      f << p.second[i] << ",";
-    }
-    f << p.second.back() << "};\n";
+}
 
-  }*/
-  fprintf(f,"Plane Surface(1)={1:%i};\n",ill-1);
-  fprintf(f,"Mesh.CharacteristicLengthExtendFromBoundary = 0;\n");
-  fprintf(f,"Mesh.CharacteristicLengthFromPoints = 0;\n");
-  fclose(f);
-  free(neighbours);
+static void orient_triangles(const double *x, int n_tri, int *tri) {
+  for (int i=0; i<n_tri; ++i){
+    int *t = tri+i*3;
+    if (triangle_signed_area(x+t[0]*2,x+t[1]*2,x+t[2]*2) < 0) {
+      int sw = t[0];
+      t[0] = t[1];
+      t[1] = sw;
+    }
+  }
+}
+
+static int find_triangle_containing_point(const double *x, int n_tri, const int *tri, double lon, double lat) {
+  // find triangle with first point
+  int first = -1;
+  double xp[2] = {lon,lat};
+  for (int i=0; i<n_tri; ++i) {
+    const int *t = tri+i*3;
+    if(is_inside(xp,x+t[0]*2,x+t[1]*2,x+t[2]*2)) {
+      first = i;
+    }
+  }
+  if(first==-1) {
+    printf("no triangle contains the first point\n");
+    exit(1);
+  }
+  return first;
+}
+
+static int *color_triangles(const double *x, int n_tri, const int *tri, const int *neigh, double lon, double lat, const double *length) {
+  int first = find_triangle_containing_point(x,n_tri,tri,lon,lat);
+  int *tri_color = malloc(sizeof(int)*n_tri);
+  for (int i=0; i<n_tri; ++i) {
+    tri_color[i] = 0;
+  }
+  int *stack = malloc(sizeof(int)*n_tri);
+  int stack_p = 0;
+  stack[0] = first;
+  tri_color[first] = 1;
+  while(stack_p >= 0) {
+    int c = stack[stack_p--];
+    for (int j=0; j<3; ++j) {
+      int n = neigh[c*3+j];
+      if (n >=0 && !tri_color[n]) {
+        int i0 = tri[c*3+j];
+        int i1 = tri[c*3+(j+1)%3];
+        const double *p0 = x+2*i0;
+        const double *p1 = x+2*i1;
+        double d2 = (p1[0]-p0[0])*(p1[0]-p0[0])+(p1[1]-p0[1])*(p1[1]-p0[1]);
+        double lmin = fmin(length[i0],length[i1]);
+        if(d2 > lmin*lmin) {
+          tri_color[n] = 1;
+          stack[++stack_p] = n;
+        }
+      }
+    }
+  }
+  free(stack);
+  return tri_color;
+}
+
+static void optimize_mesh(double *x, double *length, int nx, int n_tri, int *tri, int *tri_color, int *neigh) {
+  int *vcolor = malloc(sizeof(int)*nx);
+  for (int i=0; i<nx; ++i) {
+    vcolor[i] = 0;
+  }
+  for (int i=0; i<n_tri; ++i) {
+    if (tri_color[i] == 1) {
+      for (int j=0; j<3; ++j) {
+        vcolor[tri[i*3+j]] = 1;
+      }
+    }
+  }
+
+  //swap - collapses
+  size_t nops ;
+  int iter = 0;
+  do {
+    iter += 1;
+    nops = 0;
+    for (int i=0;i<n_tri;i++) {
+      int *t = tri+i*3;
+      for (int j=0; j<3 && t[0] != -1;++j) {
+        int n = neigh[i*3+j];
+        if (n != -1 && tri_color[n] == tri_color[i]){
+          //nops += swap(i,j,tri,x,neigh);
+        }
+        int i0 = t[j];
+        int i1 = t[(j+1)%3];
+        if(tri_color[i] == 0 && (n == -1 || tri_color[n] !=1)) {
+          // we are outside the domain
+          // and so is our neighbour (if any)
+          // remove point that does not touch the boundary
+          if (vcolor[i1] == 0) {
+            int r = collapse_edge(i,j,1,tri,neigh,x,length,vcolor);
+            nops += r;
+          }
+          else if (vcolor[i0] == 0){
+            int r = collapse_edge(i,j,0,tri,neigh,x,length,vcolor);
+            nops += r;
+          }
+        }
+        if(tri_color[i] == 1 && (n == -1 || tri_color[n] ==0)
+            && vdist(&x[i0*2],&x[i1*2]) < 0.7*fmin(length[i0],length[i1])) {
+          // we are on the boundary
+          // collapse edge if too short (and possible)
+          nops += 
+            collapse_edge(i,j,optimal_boundary_collapse(i,j,tri,neigh,x,tri_color),tri,neigh,x,length,vcolor) ||
+            collapse_edge(i,j,0,tri,neigh,x,length,vcolor) || collapse_edge(i,j,1,tri,neigh,x,length,vcolor);
+        }
+      }
+    }
+  } while (nops != 0 && iter < 100);
+  free(vcolor);
+}
+
+void gen_boundaries_from_points(int n_vertices, double *x, int *tag, int n_tri, int *tri, double lon, double lat, double *mesh_size, double **xo, int *nxo, int **l, int *nl, int **ll, int *nll)
+{
+  orient_triangles(x,n_tri,tri);
+  int *neigh = build_neighbours(n_tri,tri);
+  int *tri_color = color_triangles(x,n_tri,tri,neigh,lon,lat,mesh_size);
+  optimize_mesh(x, mesh_size, n_vertices, n_tri, tri, tri_color, neigh);
+  extract_boundaries(x, tag, n_tri, tri, tri_color, neigh/*, std::map<int,std::string> boundary_names*/,xo,  nxo, l,nl,ll,nll);
+  free(tri_color);
+  free(neigh);
+}
+
+void libcfree(void *p) {
+  free(p);
 }
--- seamesh.py
+++ seamesh.py
@@ -3,6 +3,11 @@ import ctypes as c
 from ctypes.util import find_library
 import os
 import platform 
+from gdal import ogr,osr
+
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 libdir = os.path.dirname(os.path.realpath(__file__))
 if platform.system() == "Windows":
     libpath = os.path.join(libdir, "unref.dll")
@@ -16,16 +21,37 @@ lib = c.CDLL(libpath)
 import numpy as np
 import ctypes as c
 
-def read_shp(filename, lcmin, ellps) :
-    n = c.c_int()
-    xp = c.POINTER(c.c_double)()
-    tagp = c.POINTER(c.c_int)()
-    lib.read_shp(filename.encode(),c.c_double(lcmin),ellps.encode(),c.byref(n),c.byref(xp),c.byref(tagp))
-    x = np.ctypeslib.frombuffer(c.cast(xp,c.POINTER(n.value*3*c.c_double)).contents).reshape([-1,3]).copy()
-    tag = np.ctypeslib.frombuffer(c.cast(tagp,c.POINTER(n.value*c.c_int)).contents,dtype=np.int32).copy()
-    lib.libcfree(xp)
-    lib.libcfree(tagp)
-    return x,tag
+def read_points_from_shp(filename,lcmin,ellps) :
+    points = []
+    tags = []
+    proj = osr.SpatialReference()
+    #proj.ImportFromProj4("+ellps=WGS84 +proj=utm +utm_zone=31")
+    proj.ImportFromProj4("+ellps=WGS84 +proj=geocent")
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    data = driver.Open(filename,0)
+    layer = data.GetLayer()
+    layerproj = layer.GetSpatialRef()
+    reprojector = osr.CoordinateTransformation(layerproj,proj)
+    for i in layer :
+        try :
+            phys = i.GetField("entity")
+        except :
+            phys = -1
+        g = i.GetGeometryRef()
+        lastp = None
+        pts = np.array(reprojector.TransformPoints(g.GetPoints()))
+        d=pts[1:,:]-pts[:-1,:]
+        l=np.linalg.norm(d,axis=1)
+        n=np.ceil(l/lcmin).astype(np.int32)
+        for i in range(pts.shape[0]-1) :
+            for j in range(n[i]):
+                alpha = j/n[i]
+                points.append((1-alpha)*pts[i]+alpha*pts[i+1])
+                tags.append(phys)
+        points.append(pts[-1])
+        tags.append(phys)
+    return np.array(points),np.array(tags)
+
 
 def np2c(a,dtype=np.float64) :
     tmp = np.require(a,dtype,"C")
@@ -33,28 +59,27 @@ def np2c(a,dtype=np.float64) :
     r.tmp = tmp
     return r
 
-def gen_boundaries(x,tag,tri,lon,lat,mesh_size_cb,lcmin) :
-    n_tri_p = c.c_int()
-    n_points_p = c.c_int()
-    x_p = c.POINTER(c.c_double)()
-    tri_o_p = c.POINTER(c.c_int)()
-    tri_color_p = c.POINTER(c.c_int)()
+def gen_boundaries(x,tag,tri,lon,lat,mesh_size_cb) :
+    n_ll = c.c_int()
+    n_l = c.c_int()
+    n_xo = c.c_int()
+    p_xo = c.POINTER(c.c_double)()
+    p_l = c.POINTER(c.c_int)()
+    p_ll = c.POINTER(c.c_int)()
+    mesh_size = np.array([mesh_size_cb(xi[0],xi[1],0)for xi in x])
     cb = c.CFUNCTYPE(c.c_double,c.c_double,c.c_double,c.c_double)(mesh_size_cb)
-    lib.gen_boundaries(
+    lib.gen_boundaries_from_points(
             c.c_int(x.shape[0]),np2c(x),np2c(tag,np.int32),
-            c.c_int(tri.simplices.shape[0]),np2c(tri.simplices,np.int32),np2c(tri.neighbors,np.int32),
+            c.c_int(tri.shape[0]),np2c(tri,np.int32),
             c.c_double(lon),c.c_double(lat),
-            cb,c.c_double(lcmin),
-            c.byref(n_tri_p),c.byref(tri_o_p),c.byref(tri_color_p),
-            c.byref(n_points_p), c.byref(x_p))
-    tri_o = np.ctypeslib.frombuffer(c.cast(tri_o_p,c.POINTER(n_tri_p.value*3*c.c_int)).contents,dtype=np.int32).reshape([-1,3]).copy()
-    tri_color_o = np.ctypeslib.frombuffer(c.cast(tri_color_p,c.POINTER(n_tri_p.value*c.c_int)).contents,dtype=np.int32).copy()
-    x_o = np.ctypeslib.frombuffer(c.cast(x_p,c.POINTER(n_points_p.value*2*c.c_double)).contents,dtype=np.float64).reshape([-1,2]).copy()
-    lib.libcfree(tri_o_p)
-    lib.libcfree(tri_color_p)
-    lib.libcfree(x_p)
-    return x_o,tri_o,tri_color_o
-
-def write_geo(filename,lc, x, tag, tri, tri_color) :
-    lib.write_geo(filename.encode(),c.c_double(lc),np2c(x),np2c(tag,np.int32),c.c_int(tri.shape[0]),np2c(tri,np.int32),np2c(tri_color,np.int32))
-#void write_geo(const char *filename, double lc, int n_tri, const int *tri, const int *tri_color, int n_points, const double *x_p, const int *vtag/*, std::map<int,std::string> boundary_names*/)
+            np2c(mesh_size),
+            c.byref(p_xo),c.byref(n_xo),
+            c.byref(p_l),c.byref(n_l),
+            c.byref(p_ll),c.byref(n_ll))
+    xo = np.ctypeslib.frombuffer(c.cast(p_xo,c.POINTER(n_xo.value*2*c.c_double)).contents,dtype=np.float64).reshape([-1,2]).copy()
+    lib.libcfree(p_xo)
+    l = np.ctypeslib.frombuffer(c.cast(p_l,c.POINTER(n_l.value*3*c.c_int)).contents,dtype=np.int32).reshape([-1,3]).copy()
+    lib.libcfree(p_l)
+    ll = np.ctypeslib.frombuffer(c.cast(p_ll,c.POINTER(n_ll.value*2*c.c_int)).contents,dtype=np.int32).reshape([-1,2]).copy()
+    lib.libcfree(p_ll)
+    return xo,l,ll
