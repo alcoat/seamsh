6defd3e8c291db4e746e1ca2190c1d7a9b309f40
Jonathan Lambrechts <jonathan.lambrechts@uclouvain.be>
2020-07-02 18:19:27 +0200

    add licenses files

--- seamsh/gmsh.py
+++ seamsh/gmsh.py
@@ -1,3 +1,23 @@
+# seamsh - Copyright (C) <2010-2020>
+# <Universite catholique de Louvain (UCL), Belgium
+# 	
+# List of the contributors to the development of seamsh: see AUTHORS file.
+# Description and complete License: see LICENSE file.
+# 	
+# This program (seamsh) is free software: 
+# you can redistribute it and/or modify it under the terms of the GNU Lesser General 
+# Public License as published by the Free Software Foundation, either version
+# 3 of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program (see COPYING file).  If not, 
+# see <http://www.gnu.org/licenses/>.
+
 import gmsh
 import numpy as np
 import uuid
